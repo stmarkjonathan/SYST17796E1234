f4882ba0b5857a42ce21911038cd3c7398a4fcc6
stmarkj <stmarkj@sheridanc.on.ca>
2022-02-03 12:09:47 -0500

    calcAvg code

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main{
+    public static void main(String[] args) {
+        
+        int[] temperature = new int[14];
+
+        for (int i = 0; i < temperature.length; i++) {
+            temperature[i] = (int)(Math.random()*201-100);
+            System.out.println(temperature[i]);
+        }
+
+        System.out.printf("Average is equal to: %.2f", calcAvg(temperature));
+
+    }
+
+    public static double calcAvg(int[] temp){
+
+        double total = 0.0;
+        int allowedNum = 0;
+
+
+        for (int i : temp) {
+            if(i < 50 && i > -50){
+                total += i;
+                allowedNum++;
+            }
+        }
+
+        return total / allowedNum;
+    }
+}   
